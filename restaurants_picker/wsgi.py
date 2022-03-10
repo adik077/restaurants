149d55067cde1fbadaa9ed216fb8b251515c0921
Adrian <aginalski112@gmail.com>
2022-03-11 00:10:21 +0100

    Updated Settings2

--- restaurants_picker/wsgi.py
+++ restaurants_picker/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+path = "/home/restaurantspickerapp.pythonanywhere.com/restaurants_picker"
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "myproject.settings"
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurants_picker.settings')
 
 application = get_wsgi_application()
