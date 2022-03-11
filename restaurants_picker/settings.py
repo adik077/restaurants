f0c8a36549fd733c6d3b29d46d130444b2b0cc1b
Adrian <aginalski112@gmail.com>
2022-03-11 10:40:51 +0100

    WSGI Update2

--- restaurants_picker/settings.py
+++ restaurants_picker/settings.py
@@ -110,9 +110,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['static']
+#STATICFILES_DIRS = ['static']
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:
+    STATIC_ROOT = '/home/restaurantspickerapp/restaurantspickerapp.pythonanywhere.com/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
