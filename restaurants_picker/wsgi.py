6f01637ec5da58063d8bb5ec4664f7c949b38604
Adrian <aginalski112@gmail.com>
2022-03-11 10:10:04 +0100

    WSGI Update

--- restaurants_picker/wsgi.py
+++ restaurants_picker/wsgi.py
@@ -11,7 +11,7 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = "/home/restaurantspickerapp/restaurants_picker"
+path = "/home/restaurantspickerapp/restaurantspickerapp.pythonanywhere.com/restaurants_picker"
 if path not in sys.path:
     sys.path.insert(0, path)
 
